89b4cf25b5720e228c07a40bd5e82f08d6c05365
DerBrunoIR <bruno-ignaz.rothschild@campus.tu-berlin.de>
2023-07-29 21:01:36 +0200

    removed trash

--- lua/trash/setup-cmp-nvim-ultisnips.lua
+++
@@ -1,8 +0,0 @@
-
-require("cmp_nvim_ultisnips").setup {
-  filetype_source = "ultisnips_default",
-  show_snippets = "all",
-  documentation = function(snippet)
-    return snippet.description .. "\n\n" .. snippet.value
-  end
-}
