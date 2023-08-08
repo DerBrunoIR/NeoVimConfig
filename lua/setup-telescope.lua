c86cef11a1fe6b629fb42774a34592408b30ecae
DerBrunoIR <bruno.ignaz.rothschild@gmail.com>
2023-08-08 20:04:40 +0200

    f

--- lua/setup-telescope.lua
+++ lua/setup-telescope.lua
@@ -2,7 +2,7 @@
 
 require('telescope').setup{
   defaults = {
-    -- Default configuration for telescope goes here:
+    -- Default configuration for telescope goes here
     -- config_key = value,
     mappings = {
       i = {
@@ -31,4 +31,8 @@ require('telescope').setup{
   }
 }
 
-
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
