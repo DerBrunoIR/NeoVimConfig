57547351e8ac921a9b5e94c2dfa9fa7157cc9104
Bruno Ignaz Rothschild <bruno-ignaz.rothschild@campus.tu-berlin.de>
2024-02-20 05:11:13 +0100

    edit lsp serveres

--- lua/setup-language-servers.lua
+++ lua/setup-language-servers.lua
@@ -1,15 +1,9 @@
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+
 require('lspconfig')['asm_lsp'].setup { capabilities = capabilities }
 require('lspconfig')['clangd'].setup { capabilities = capabilities }
-require('lspconfig')['awk_ls'].setup { capabilities = capabilities }
-require('lspconfig')['bashls'].setup { capabilities = capabilities }
-require('lspconfig')['cmake'].setup { capabilities = capabilities }
-require('lspconfig')['cssls'].setup { capabilities = capabilities }
-require('lspconfig')['dockerls'].setup { capabilities = capabilities }
-require('lspconfig')['eslint'].setup { capabilities = capabilities }
 require('lspconfig')['gopls'].setup {
 	capabilities = capabilities,
 	settings = {
@@ -27,9 +21,20 @@ require('lspconfig')['gopls'].setup {
 		usePlaceholders = true,
 	}
 }
-require('lspconfig')['jsonls'].setup { capabilities = capabilities }
 require('lspconfig')['jdtls'].setup { capabilities = capabilities }
-require('lspconfig')['tsserver'].setup { capabilities = capabilities }
+require('lspconfig')['pyright'].setup { capabilities = capabilities }
+require('lspconfig')['rust_analyzer'].setup { capabilities = capabilities }
+require('lspconfig')['lua_ls'].setup { capabilities = capabilities }
+require('lspconfig')['vimls'].setup { capabilities = capabilities }
+require('lspconfig')['eslint'].setup { capabilities = capabilities }
+
+require('lspconfig')['cmake'].setup { capabilities = capabilities }
+require('lspconfig')['autotools_ls'].setup { capabilities = capabilities }
+require('lspconfig')['bashls'].setup { capabilities = capabilities }
+require('lspconfig')['awk_ls'].setup { capabilities = capabilities }
+require('lspconfig')['marksman'].setup { capabilities = capabilities }
+require('lspconfig')['dockerls'].setup { capabilities = capabilities }
+require('lspconfig')['compose-language-service'].setup { capabilities = capabilities }
 -- documentation for ltex at https://valentjn.github.io/ltex/settings.html
 require('lspconfig')['ltex'].setup {
 	capabilities = capabilities,
@@ -43,9 +48,5 @@ require('lspconfig')['ltex'].setup {
 		},
 	},
 }
---require('lspconfig')['marksman'].setup { capabilities = capabilities }
-require('lspconfig')['pyright'].setup { capabilities = capabilities }
 require('lspconfig')['sqlls'].setup { capabilities = capabilities }
-require('lspconfig')['rust_analyzer'].setup { capabilities = capabilities }
-require('lspconfig')['lua_ls'].setup { capabilities = capabilities }
-
+require('lspconfig')['cssls'].setup { capabilities = capabilities }
