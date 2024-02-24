a6295e3a6b5dfdd0ec5024182ca7916449875858
Bruno Rothschild <bruno.iganz.rothschild@gmail.com>
2024-02-24 19:43:34 +0100

    edit default treesitter parsers

--- lua/setup-treesitter.lua
+++ lua/setup-treesitter.lua
@@ -2,6 +2,10 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = {
+	  "vim",
+	  "lua",
+	  "c",
+	  "go",
   },
   -- ensure_installed = { "all" },
 
