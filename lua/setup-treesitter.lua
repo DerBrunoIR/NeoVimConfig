ea215870027e1635754cdaa0f18589445f8c9de8
Bruno Rothschild <bruno.iganz.rothschild@gmail.com>
2024-02-24 20:07:56 +0100

    add mandotory ts parsers to config

--- lua/setup-treesitter.lua
+++ lua/setup-treesitter.lua
@@ -2,10 +2,11 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = {
-	  "vim",
-	  "lua",
 	  "c",
-	  "go",
+	  "vim",
+	  "vimdoc",
+	  "query",
+	  "lua",
   },
   -- ensure_installed = { "all" },
 
