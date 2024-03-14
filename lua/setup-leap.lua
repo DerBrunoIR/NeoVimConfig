c04598d01f802609e63b8ed1fd3e40503d9e30a8
DerBrunoIR <95578637+DerBrunoIR@users.noreply.github.com>
2024-03-14 18:34:17 +0100

    Update setup-leap.lua
    change s and S in leap keybindings

--- lua/setup-leap.lua
+++ lua/setup-leap.lua
@@ -1,6 +1,6 @@
 
-vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-backward)' )
-vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-forward)')
+vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)' )
+vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward)')
 vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)' )
 
 local opts = require('leap').opts
